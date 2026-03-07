023bd3807c05f3be66d26fa5895aa4a572ace4ac
David Johnson <drj10101@gmail.com>
2026-03-07 13:32:54 -0700

    Line endings

--- example/Program.cs
+++ example/Program.cs
@@ -4,28 +4,15 @@ using jbSoft.Reusable;
 
 public class SimpleWebServer
 {
-  public static async Task<int> Main(string[] args)
+  public static int Main(string[] args)
   {
     SelfHostWebLog.WriteLine = Console.WriteLine;
-    CancellationTokenSource cancellationTokenSource = new();
-
-    AppDomain.CurrentDomain.ProcessExit += async (sender, eventArgs) =>
-    {
-      SelfHostWebLog.WriteLine("ProcessExit signal received. Initiating shutdown...");
-      cancellationTokenSource.Cancel();
-    };
-
-    Console.CancelKeyPress += async (sender, eventArgs) =>
-    {
-      SelfHostWebLog.WriteLine("Ctrl+C signal received. Initiating shutdown...");
-      cancellationTokenSource.Cancel();
-    };
-
+    
     var server = new HttpServer(7000);
 
     Shutdown.AddRestartUrl = true;
 
-    await server.Start(cancellationTokenSource);
+    server.Start();
 
     return 0;
   }
